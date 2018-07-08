2f6aaae06b6ce0655a95e8620890311bbfc6657b
kyle lee <suhan.lee.k@gmail.com>
2018-07-09 00:58:46 +0900

    add TextInput

--- MediaExam/App.js
+++ MediaExam/App.js
@@ -7,27 +7,41 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View, TextInput} from 'react-native';
 import Video from 'react-native-video';
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
+    }
+  }
+
   videoError = (error) => {
     console.log(error);
   };
+
   render() {
-    const url = 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4';
 
     return (
       <View style={styles.container}>
-        <Video source={{uri: url}}   // Can be a URL or a local file.
+        <Video source={{uri: this.state.url}}   // Can be a URL or a local file.
                ref={(ref) => {
                  this.player = ref
                }}
                onError={this.videoError}
                style={styles.backgroundVideo}
-              />
-
+        />
+        <TextInput
+          style={{
+            width: 300,
+            height: 40}}
+          placeholder="Type input"
+          onChangeText={(text) => this.setState({url: text})}
+        />
       </View>
     );
   }
@@ -40,10 +54,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   backgroundVideo: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
+    width: 200,
+    height: 200,
   },
 });
